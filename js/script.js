aa615a4f8c20fa5129c96dcd8e7f26150f96f7f4
ashikurdev <ashikgsc170@gmail.com>
2024-09-17 16:53:55 +0600

    scroll2top button added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-document.getElementById('scrollToTop').addEventListener('click', function() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+// Scroll to top button settings
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 400) {
+    scrollToTopButton.classList.remove('hidden');
+  } else {
+    scrollToTopButton.classList.add('hidden');
+  }
+});
+
+scrollToTopButton.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+});
